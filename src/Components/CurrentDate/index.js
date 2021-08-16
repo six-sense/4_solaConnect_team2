1de56f99e0b4dcab17ebc42bf8364834f99e422f
UlongChaS2 <jyujung2@gmail.com>
2021-08-16 23:04:32 +0900

    Feat: CurrentDate layout

---
+++ src/Components/CurrentDate/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function CurrentDate() {
+  return (
+    <Container>
+      <p>🗓</p>
+      <DateForm>가나다라</DateForm>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  ${({ theme }) => theme.flexSet('flex-start')}
+
+  p {
+    font-size: 50px;
+    margin-right: 10px;
+  }
+`;
+
+const DateForm = styled.div`
+  font-size: 30px;
+`;
