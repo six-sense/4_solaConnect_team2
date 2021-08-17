ade70d6ef6875a9ce5579fb6f993d9789ebbf584
UlongChaS2 <jyujung2@gmail.com>
2021-08-17 21:54:03 +0900

    Refactor: change state to constants
    There was no need to use the state, so I changed it to a variable and proceeded.

--- src/utils/getCurrentDate.js
+++ src/utils/getCurrentDate.js
@@ -1,8 +1,26 @@
+import { koDay, usDay, usMonth } from 'utils/constants';
+
 const today = new Date();
 
-export const dateObj = {
+const dateObj = {
   year: today.getFullYear(),
   month: today.getMonth() + 1,
   date: today.getDate(),
   day: today.getDay(),
+};
+
+const { year, month, date, day } = dateObj;
+
+const changeDay = (day, country) =>
+  country === 'ko' ? koDay[day] : usDay[day];
+const changeUsMonth = (month) => usMonth[month];
+
+export const sectionCountry = (country) => {
+  let countryDate;
+  country === 'ko'
+    ? (countryDate = `${year}년 ${month}월 ${date}일 ${changeDay(day)} `)
+    : (countryDate = `${changeDay(day)} ${changeUsMonth(
+        month,
+      )} ${date}, ${year}`);
+  return countryDate;
 };
