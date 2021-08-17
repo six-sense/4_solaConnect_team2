cbdc724b9446ead298151e4c5a03efa3e32f5c17
UlongChaS2 <jyujung2@gmail.com>
2021-08-17 16:14:29 +0900

    Feat: add functions that differ by country

--- src/Components/CurrentDate/index.js
+++ src/Components/CurrentDate/index.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { koDay, usDay, usMonth } from 'utils/constants';
+import { dateObj } from 'utils/getCurrentDate';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-export default function CurrentDate() {
+CurrentDate.propTypes = {
+  country: PropTypes.string,
+};
+
+export default function CurrentDate({ country }) {
+  const [currentDate, setCurrentDate] = useState('');
+  const { year, month, date, day } = dateObj;
+
+  const changeDay = (day) => (country === 'ko' ? koDay[day] : usDay[day]);
+  const changeUsMonth = (month) => usMonth[month];
+
+  const divideCountry = () => {
+    country === 'ko'
+      ? setCurrentDate(`${year}년 ${month}월 ${date}일 ${changeDay(day)} `)
+      : setCurrentDate(
+          `${changeDay(day)} ${changeUsMonth(month)} ${date}, ${year}`,
+        );
+  };
+  useEffect(() => {
+    divideCountry();
+  }, []);
+
   return (
     <Container>
       <p>🗓</p>
-      <DateForm>가나다</DateForm>
+      <DateForm>{currentDate}</DateForm>
     </Container>
   );
 }
